98a506a4e3e3c5926b08a33b13eb78e2be7104bf
Tova <77296521+Tovi10@users.noreply.github.com>
2021-10-10 19:07:15 +0300

    nothing

--- server.js
+++ server.js
@@ -3,4 +3,3 @@ const { http } = require('./app');
 http.listen(process.env.PORT || 4000, function () {
     console.log("Express server listening on port %d in -- mode", this.address().port);
 });
-
