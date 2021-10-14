a0fc19cef5ea5fdc0fc1b76b87cc2aa10abfdb29
Tova <77296521+Tovi10@users.noreply.github.com>
2021-10-14 11:55:13 +0300

    try again

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ app.get("/", (req, res, next) => {
     // res.status(200).send('<h1>hello</h1>');
     // app.use(express.static(path.join(__dirname, './build')));
 });
+app.get("/about", (req, res, next) => {
+    // if (req.url.includes("home")) {
+    console.log("success!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    res.sendFile(path.join(__dirname, "./build/index.html"));
+    // res.status(200).send('<h1>hello</h1>');
+    // app.use(express.static(path.join(__dirname, './build')));
+});
 // app.get("/all-campaigns", (req, res, next) => {
 //     res.redirect(`https://matching-try.herokuapp.com/all-campaigns`);
 // });
