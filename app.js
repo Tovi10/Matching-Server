2e161602cf3ad891d01655e3e381132602841e11
Tova <77296521+Tovi10@users.noreply.github.com>
2021-10-28 12:04:08 +0300

    socket

--- app.js
+++ app.js
@@ -5,36 +5,25 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 const path = require("path");
 
-// const { socketServer } = require('./webSocket');
-// socketServer(http);
+const { socketServer } = require('./webSocket');
+socketServer(http);
 const { connectDB } = require('./db');
 connectDB();
-// const userRouter = require('./routes/user.route');
 const campaignRouter = require('./routes/campaign.route');
 const giftRouter = require('./routes/gift.route');
 const cardRouter = require('./routes/card.route');
 const companyRouter = require('./routes/company.route');
 const userRouter = require('./routes/user.route');
 const donationRouter = require('./routes/donation.route');
-const recruiterRouter=require('./routes/recruiter.route');
-const applyRouter=require('./routes/apply.route');
+const recruiterRouter = require('./routes/recruiter.route');
+const applyRouter = require('./routes/apply.route');
 
 app.use(express.static('public'));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//NOTE SERVER
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, './build')));
-// app.use(express.static(path.join(__dirname, '../build')));
-//NOTE SERVER
-// app.get("/", (req, res, next) => {
-//     // if (req.url.includes("home")) {
-//     console.log("success@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-//     res.sendFile(path.join(__dirname, "./build/index.html"));
-//     // res.status(200).send('<h1>hello</h1>');
-//     // app.use(express.static(path.join(__dirname, './build')));
-// });
+
 app.get("/about", (req, res, next) => {
     res.sendFile(path.join(__dirname, "./build/index.html"));
 });
@@ -47,9 +36,6 @@ app.get("/about", (req, res, next) => {
 app.get("/all-campaigns", (req, res, next) => {
     res.sendFile(path.join(__dirname, "./build/index.html"));
 });
-// app.get("/curren-campaign", (req, res, next) => {
-//     res.sendFile(path.join(__dirname, "./build/index.html"));
-// });
 app.get("/current-campaign/:currentCampaign", (req, res, next) => {
     res.sendFile(path.join(__dirname, "./build/index.html"));
 });
@@ -66,7 +52,6 @@ app.get("/gift-details/:currentGift", (req, res, next) => {
     res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
-// app.use('/api/user', userRouter);
 app.use('/api/campaign', campaignRouter);
 app.use('/api/gift', giftRouter);
 app.use('/api/card', cardRouter);
