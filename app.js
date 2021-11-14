3e499f567d47b0fe29eb9e3360632d51ae9c1acc
shulamit-leader <74709557+shulamit8562@users.noreply.github.com>
2021-11-14 19:07:37 +0200

    try #3

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.get("/thank/:x*", (req, res, next) => {
     console.log('************************');
     console.log(req.url.split('&')[1].split('response=')[1])
     // https://matching-try.herokuapp.com/thank/4&response=failure&json=eyJlcnIiOnsiaWQiOjM0MywibWVzc2FnZSI6Iteg15Ag15zXpNeg15XXqiDXnNee16DXlNecINee16LXqNeb16ogMzE0NCoifSwic3RhdHVzIjoiMCIsImRhdGEiOnsiaWQiOiIxMzcxNTYwMSIsIm5ld19wYXltZW50X2lkIjoxMzcxNTYxMX19&data_url=https://cgmpi.creditguard.co.il/CGMPI_Server/PerformTransaction
-    res.sendFile(path.join(__dirname, "./build/index.html"));
+    res.sendFile(path.join(__dirname, "./failure.html"));
 });
 
 
