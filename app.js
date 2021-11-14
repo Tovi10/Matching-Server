fd3289d2cc127076046ada7006b74fe08e5195d5
shulamit-leader <74709557+shulamit8562@users.noreply.github.com>
2021-11-14 18:48:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -70,6 +70,9 @@ app.get("&response=failure&json=eyJlcnIiOnsiaWQiOjM0MywibWVzc2FnZSI6Iteg15Ag15zX
 });
 app.get("/thank/:x*", (req, res, next) => {
     console.log('************************');
+    console.log(req+"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP" )
+    // https://matching-try.herokuapp.com/thank/4&response=failure&json=eyJlcnIiOnsiaWQiOjM0MywibWVzc2FnZSI6Iteg15Ag15zXpNeg15XXqiDXnNee16DXlNecINee16LXqNeb16ogMzE0NCoifSwic3RhdHVzIjoiMCIsImRhdGEiOnsiaWQiOiIxMzcxNTYwMSIsIm5ld19wYXltZW50X2lkIjoxMzcxNTYxMX19&data_url=https://cgmpi.creditguard.co.il/CGMPI_Server/PerformTransaction
+    
     res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
